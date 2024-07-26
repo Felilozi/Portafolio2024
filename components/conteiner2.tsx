490a24fb5fb6ab565096492aa736c1c6c1321aa8
Felilozi <felicialozinski97@gmail.com>
2024-07-26 14:51:17 -0300

    Actualizando2.7

--- components/conteiner2.tsx
+++ components/conteiner2.tsx
@@ -9,7 +9,7 @@ const ConteinerPage1 = (props:ContainerProps)=>{
     const {children}= props
     return (
         <div className=" w-full max-w-6xl px-4 pb-36
-        mx-auto mt-32 md:pb-30 md: md:mt-30
+        mx-auto mt-32 md:pb-24 md: md:mt-16 lg:mt-30
         ">{children}
         </div>
         //w-full max-w-6xl px-4 pb-40 mx-auto mt-40 md:pb-0 md:px-6
